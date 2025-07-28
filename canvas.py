6397a8ad8a861ef757ba7c7ff76e0c3d4ed9ceed
= <sultaninstem004@gmail.com>
2025-07-27 21:16:34 -0700

    canvas

--- canvas.py
+++ canvas.py
@@ -1,4 +1,5 @@
 import pygame
+import sys 
 class Canvas: 
     def __init__(self):
         pygame.init()
@@ -8,17 +9,24 @@ class Canvas:
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption("Hilbert curve")
         self.fps = 30 
+        self.clock = pygame.time.Clock()
 
     def update(self): 
         pass 
     def handle_events(self): 
         for event in pygame.event.get(): 
-            if event == pygame.QUIT: 
+            if event.type == pygame.QUIT: 
                 self.running = False 
     def display(self): 
-        pass 
+        self.screen.fill((0,0,0))
+
+        pygame.display.flip()
+        self.clock.tick(self.fps)
     def run(self):
         while(self.running):
             self.update()
             self.handle_events()
             self.display() 
+        pygame.quit()
+        sys.exit()
+
